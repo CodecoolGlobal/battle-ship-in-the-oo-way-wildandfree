95c9c036dd7b55be82a2e63a15d83fb035802851
chash <chashafe@gmail.com>
2020-02-11 19:41:28 +0100

    pulling1

---
+++ Ship.cs
@@ -0,0 +1,42 @@
+// using System;
+// using System.Collections.Generic;
+
+
+// namespace battle_ship_in_the_oo_way_wildandfree
+// {
+//     public class Ship
+//     {
+//         List<string> ship1 = new List<string>();
+//         int shipLength;
+//         string orientation; 
+//         // Tuple <string, int> position = new Tuple <string, int>();
+//     public void BuildShip()
+//     {
+//         for (int i=0; i<shipLength; i++)
+//         {
+//             ship1.Add(" ");
+//         }
+//     }
+//     public void OrientShip(char letter)
+//     {
+//         if (letter == 'V')
+//         {
+//             orientation = "vertical";
+//         }
+//         else if (letter == 'H')
+//         {
+//             orientation = "horizontal";
+//         }
+//         else {{}}
+//     }
+//     // public override string ToString()
+//     // {
+//     //     square smallSquare = new square();
+        
+//     //     for (int i=0; i< ship1.Count; i++)
+//     //     {
+//     //         return smallSquare.DrawSquare();
+//     //     }
+//     // }
+//     }
+// }
